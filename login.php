21b9ded913ca04ddc612fed885f03746a56ccd87
Paola Dueña <43018406+PaoRioColorado@users.noreply.github.com>
2022-04-17 20:22:34 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+//Primero le doy un nombre a la variable, en este caso le di nombre user
+$user= $_POST ["user"]; //Trae el dato que tenga en el input de nombre "user" del frontend
+$pass= $_POST ["pass"];
+
+$usuario="admin";
+$contrasenia= "1234";
+
+if ($usuario=="$user" && $contrasenia=="$pass") {
+    header ("location: https://careers-meli.mercadolibre.com/");
+} else {
+    header("location:error.html");
+  }
+  
+  
+   ?>
